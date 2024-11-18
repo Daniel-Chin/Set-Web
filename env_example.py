68505c02caa2482305948ab15a114e851b8477fc
Daniel Chin <daniel.chin@nyu.edu>
2024-11-18 11:56:18 +0400

    update env exmaple

--- env_example.py
+++ env_example.py
@@ -6,23 +6,35 @@ TEXTURE_RESOLUTION = 2160
 
 GLOBAL_SCALING = 1.6
 
-FONT_SIZE = round(121 * GLOBAL_SCALING)
+FONT_SIZE = round(18 * GLOBAL_SCALING)
+
+FONT = 'Helvetica'
+# FONT = 'Segoe UI'
+# FONT = 'Arial'
+# FONT = 'DejaVu Sans'
+# FONT = 'Liberation Sans'
 
 # card display width in pixels
 CARD_WIDTH = round(100 * GLOBAL_SCALING)
 
 # size multiplier for cards in the display cases
-SMALL_CARD_RATIO = 0.4
+SMALL_CARD_RATIO = 0.6
 
 # thickness indicator (width, height) in pixels
 THICKNESS_INDICATOR_WEALTH = (
     round(70 * GLOBAL_SCALING), 
-    round(70 * GLOBAL_SCALING), 
+    round(30 * GLOBAL_SCALING), 
 )
 THICKNESS_INDICATOR_DECK = (
-    round(140 * GLOBAL_SCALING), 
-    round(140 * GLOBAL_SCALING), 
+    round(300 * GLOBAL_SCALING), 
+    round(120 * GLOBAL_SCALING), 
 )
+
+# how thick is one line
+THICKNESS_INDICATOR_CARD_THICKNESS = 1
+
+# interval between lines
+THICKNESS_INDICATOR_CARD_INTERVAL = 2
 
 # padding in pixels
 PADX = round(12 * GLOBAL_SCALING)
